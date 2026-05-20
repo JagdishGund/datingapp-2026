a4fa30915cc2b5693d6a105cf6373b2dc2d04aa9
Jagdish Gund <jgund3@gmail.com>
2026-05-20 16:46:26 +0530

    End of section of 5

---
+++ client/src/core/services/account-service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../types/user';
+import { tap } from 'rxjs/internal/operators/tap';
+
+type registerCreds = Record<string, unknown>;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+
+  private http = inject(HttpClient);
+  baseUrl = 'http://localhost:5247/api/';
+
+  currentUser = signal<User | null>(null);
+
+  register(creds: registerCreds) {
+    return this.http.post<User>(`${this.baseUrl}account/register`, creds).pipe(
+      tap(user => { 
+        if (user) {
+          this.setCurrentUser(user);
+        } else {
+          console.error('Registration failed: No user returned from API');
+        }   
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+  }
+
+  login(creds: any) {
+    return this.http.post<User>(`${this.baseUrl}account/login`, creds).pipe(
+      tap(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+
+}
---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,31 @@
+import { Component, inject, Inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css',
+})
+export class Nav {
+
+  protected creds: any= {};
+  protected accountService= inject(AccountService);
+
+  login() {
+    // console.log(this.creds);
+    this.accountService.login(this.creds).subscribe({
+      next: response => {
+       console.log(response);
+       this.creds= {};
+      },
+      error: error => alert(error.message)
+    });
+  }
+
+  logout() {
+   this.accountService.logout();
+   alert('Logged out successfully');
+  }
+}
