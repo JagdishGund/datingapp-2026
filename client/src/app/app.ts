aade959283399fb27dd3a0fefe27cf634f0e1b4a
Jagdish Gund <jgund3@gmail.com>
2026-05-22 12:24:33 +0530

    Router, authguard, toast, route animation- EOS-6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,41 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-   private accountService = inject(AccountService);
-  protected  title = 'Dating App';
-  protected members = signal<any>([]);
+export class App{
 
-  constructor(private http: HttpClient) {   }
+  protected router = inject(Router);
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get('http://localhost:5247/api/members'));
-    } catch (error) {
-      console.error('Error fetching members:', error);
-      throw error; // Rethrow the error after logging it  
-    }
-  }
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,31 +1,38 @@
 import { Component, inject, Inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
 
   protected creds: any= {};
+  private router = inject(Router);
+  private toast = inject(ToastService);
   protected accountService= inject(AccountService);
 
   login() {
     // console.log(this.creds);
     this.accountService.login(this.creds).subscribe({
       next: response => {
-       console.log(response);
+       this.router.navigateByUrl('/members');
+       this.toast.success('Login successful!', 5000);
        this.creds= {};
       },
-      error: error => alert(error.message)
+      error: error => {
+        this.toast.error(error.error, 5000);
+      }
     });
   }
 
   logout() {
    this.accountService.logout();
-   alert('Logged out successfully');
+   this.router.navigateByUrl('/');
   }
 }
