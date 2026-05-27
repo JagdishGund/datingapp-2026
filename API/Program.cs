3884d2d50ea7979c083619cb3864e8f5f1ccb812
Jagdish Gund <jgund3@gmail.com>
2026-05-27 15:46:01 +0530

    Error handling on client and API

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -33,6 +34,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 // if (app.Environment.IsDevelopment())
 // {
 //     app.UseSwagger();
